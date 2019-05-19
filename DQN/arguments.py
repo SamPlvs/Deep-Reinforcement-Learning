2bbed38e7e791cf7f0e399b2394f7b32590d213e
S Tukra <35964759+SamPlvs@users.noreply.github.com>
2019-05-19 12:10:26 +0100

    Add files via upload

---
+++ DQN/arguments.py
@@ -0,0 +1,38 @@
+import argparse
+
+def get_args():
+    parse= argparse.ArgumentParser()
+    # network related arguments
+    parse.add_argument("--env_name", type=str, default="Pong-v0", help="The environment")
+    parse.add_argument("--batch_size", type= int, default= 64, help="The batch size of the data we train from")
+    parse.add_argument("--initializer", type=str, default="xavier", help= "Method of weight initialization")
+    parse.add_argument('--lr', type=float, default=0.001, help='learning rate of the algorithm')
+    parse.add_argument('--seed', type=int, default= 123, help='the seed value to initialise the pseudo random generator at the same random values')
+    parse.add_argument("--train", action= 'store_true', help="Train the model only")
+    parse.add_argument("--test", action= 'store_true', help="Test the model only (must load a model!)")
+    parse.add_argument("--save_path", type= str, default= r'/home/neurobeast/Documents/SamTukra/DRL/DQN/models/', help="The path where you want to save your trained model and for loading the same model when testing")
+    parse.add_argument("--loss_type", type= str, default= 'mse', help="The loss type you want to update the model with")
+    # algorithm related arguments
+    parse.add_argument('--gamma', type=float, default=0.8, help='the discount factor i.e. gamma')
+    parse.add_argument('--tau', type=float, default=1e-3, help='parameter for performing soft update on target net')
+    parse.add_argument("--buffer_size", type=int, default=10000, help="max memory length for the Replay Buffer")
+    parse.add_argument("--epsilon_start", type=float, default=0.9, help="starting epsilon")
+    parse.add_argument("--epsilon_end", type=float, default=0.05, help="ending epsilon")
+    parse.add_argument("--epsilon_decay", type=float, default=200, help="epsilon decay parameter")
+    # implementation related arguments
+    parse.add_argument("--clip_grads", action='store_true', help="perform gradient clipping")
+    parse.add_argument("--clip_error", action='store_true', help="perform error clipping")
+    parse.add_argument("--max_TimeSteps", type=int, default=1000, help="max time steps 1 episodes last! if terminal state is not reached the env will reset after this")
+    parse.add_argument("--target_net_update", type=int, default=10, help="The environment you want the agent to perform in")
+    parse.add_argument('--learning-starts', type=int, default=10, help='the frames start to learn')
+    parse.add_argument("--learn_frequency", type=int, default=4, help=" no.of time steps you want to print stuff in")
+    parse.add_argument("--display_interval", type=int, default=100, help=" no.of time steps you want to print stuff in")
+    parse.add_argument("--episodes", type=int, default=70000, help="The total no of episodes I want to train the model for")
+    parse.add_argument("--iter", type=int, default=1e7, help="The total no of iterations I want to train the model for")
+    parse.add_argument("--cuda", action= 'store_true', help="use GPU for training else, CPU")
+
+    args= parse.parse_args()
+
+    return args
+# CartPole-v0 
+    
---
+++ DQN/networks.py
@@ -0,0 +1,63 @@
+# Neural style transfer
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# this is an implementation pf JCJhonson and Fei fei li paper version of neural style transfer
+
+class conv2d(nn.Module):
+    def __init__(self, nc_in, nc_out, kernel_size, stride, bn=False, activation=False):
+        super(conv2d, self).__init__()
+        self.bn= bn
+        self.activation= activation
+        self.conv= nn.Conv2d(nc_in, nc_out, kernel_size, stride)
+        if self.bn == True:
+            self.norm = nn.BatchNorm2d(nc_out)
+    
+    def forward(self, x):
+        x= self.conv(x)
+        if self.bn==True:
+            x= self.norm(x)
+        if self.activation == True:
+            x= F.relu(x)
+        return x
+
+class DQN_cnn(nn.Module):
+    def __init__(self, state_input, action_output, bn_bool=False):
+        super(DQN_cnn, self).__init__()
+        """
+        state_input= input frames of the state i.e. no. of input channels of the atari frames
+        action_output= no. of output actions := no. of nodes in the final layer
+        """
+        self.conv1= conv2d(state_input, 32, 8, stride=4, bn=bn_bool, activation=True)
+        self.conv2= conv2d(32, 64, 4, stride=2, bn=bn_bool, activation=True)
+        self.conv3= conv2d(64, 32, 3, stride=1, bn=bn_bool, activation=True)
+
+        self.fcn1= nn.Linear(32*7*7, 256)
+        self.fcn2= nn.Linear(256, action_output) 
+
+    def forward(self, x):
+        x= self.conv1(x/255.0) # normalise input
+        x= self.conv2(x)
+        x= self.conv3(x)
+        x= x.view(-1, 32*7*7) # flatten the conv output to the n-dim vector same as fcn1 input
+        x= F.relu(self.fcn1(x))
+        action_Q_values = self.fcn2(x)
+
+        return action_Q_values
+
+class DQN_fcn(nn.Module):
+    def __init__(self, state_input, action_output):
+        super(DQN_fcn, self).__init__()
+        # 3 layer fully connected neural network
+        self.fcn1= nn.Linear(state_input, 128)
+        self.fcn2= nn.Linear(128, 64)
+        self.fcn4= nn.Linear(64, action_output)
+       
+    def forward(self, x):
+    
+        x= F.relu(self.fcn1(x))
+        x= F.relu(self.fcn2(x))
+        action_Q_values= self.fcn4(x)
+
+        return action_Q_values
